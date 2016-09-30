c087fc59f641498c46add037cb6335a50a4f7163
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2016-09-29 17:34:41 -0700

    Move helper code to a separate file (#13)

---
+++ parser/lib.go
@@ -0,0 +1,54 @@
+package parser
+
+import "go/token"
+
+func Klein(p Parser) Parser {
+	return func(t []*Token) []*Token {
+		for newT := p(t); newT != nil; newT = p(t) {
+			t = newT
+		}
+		return t
+	}
+}
+
+func And(ps ...Parser) Parser {
+	return func(t []*Token) []*Token {
+		for _, p := range ps {
+			newT := p(t)
+			if newT == nil {
+				return nil
+			}
+			t = newT
+		}
+		return t
+	}
+}
+
+func Maybe(p Parser) Parser {
+	return func(t []*Token) []*Token {
+		if newT := p(t); newT != nil {
+			return newT
+		}
+		return t
+	}
+}
+
+func Or(ps ...Parser) Parser {
+	return func(t []*Token) []*Token {
+		for _, p := range ps {
+			if newT := p(t); newT != nil {
+				return newT
+			}
+		}
+		return nil
+	}
+}
+
+func Basic(tok token.Token) Parser {
+	return func(t []*Token) []*Token {
+		if p := pop(&t); p == nil || p.tok != tok {
+			return nil
+		}
+		return t
+	}
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,36 +1,6 @@
 package parser
 
-import (
-	"go/scanner"
-	"go/token"
-	"testing"
-
-	"github.com/nordsieck/defect"
-)
-
-var (
-	semicolon      = &Token{tok: token.SEMICOLON, lit: "\n"}
-	semicolonSlice = []*Token{semicolon}
-	fail           = []*Token(nil)
-)
-
-func newScanner(src string) *scanner.Scanner {
-	var s scanner.Scanner
-	fset := token.NewFileSet()
-	file := fset.AddFile("", fset.Base(), len(src))
-	s.Init(file, []byte(src), nil, scanner.ScanComments)
-
-	return &s
-}
-
-func Scan(s *scanner.Scanner) []*Token {
-	var t []*Token
-	for _, tok, lit := s.Scan(); tok != token.EOF; _, tok, lit = s.Scan() {
-		t = append(t, &Token{tok: tok, lit: lit})
-	}
-	reverse(t)
-	return t
-}
+import "testing"
 
 func TestBasicLit(t *testing.T) {
 	consumes(t, BasicLit, map[string]bool{
@@ -76,65 +46,4 @@ func TestPackageName(t *testing.T) {
 		`1`: false,
 		`_`: false,
 	})
-}
-
-func TestKlein(t *testing.T) {
-	for raw, left := range map[string][]*Token{
-		`1`:     semicolonSlice,
-		`1 1 1`: semicolonSlice,
-		`1 a`:   []*Token{semicolon, {tok: token.IDENT, lit: `a`}},
-		`1 a 1`: []*Token{semicolon, {tok: token.INT, lit: `1`}, {tok: token.IDENT, lit: `a`}},
-	} {
-		toks := Scan(newScanner(raw))
-		defect.DeepEqual(t, Klein(BasicLit)(toks), left)
-	}
-}
-
-func TestAnd(t *testing.T) {
-	for raw, left := range map[string][]*Token{
-		`1`:     fail,
-		`1 1`:   semicolonSlice,
-		`1 1 1`: []*Token{semicolon, {tok: token.INT, lit: `1`}},
-	} {
-		toks := Scan(newScanner(raw))
-		defect.DeepEqual(t, And(BasicLit, BasicLit)(toks), left)
-	}
-}
-
-func TestMaybe(t *testing.T) {
-	for raw, left := range map[string][]*Token{
-		`1`:   semicolonSlice,
-		`1 1`: []*Token{semicolon, {tok: token.INT, lit: `1`}},
-		`a`:   []*Token{semicolon, {tok: token.IDENT, lit: `a`}},
-	} {
-		toks := Scan(newScanner(raw))
-		defect.DeepEqual(t, Maybe(BasicLit)(toks), left)
-	}
-}
-
-func TestOr(t *testing.T) {
-	consumes(t, Or(Basic(token.IDENT), Basic(token.INT)), map[string]bool{
-		`a`:   true,
-		`1`:   true,
-		`1.1`: false,
-	})
-}
-
-func TestBasic(t *testing.T) {
-	consumes(t, Basic(token.INT), map[string]bool{
-		`1`:   true,
-		`a`:   false,
-		`1.1`: false,
-	})
-}
-
-func consumes(t *testing.T, p Parser, m map[string]bool) {
-	for raw, match := range m {
-		toks := Scan(newScanner(raw))
-		if match {
-			defect.DeepEqual(t, p(toks), semicolonSlice)
-		} else {
-			defect.DeepEqual(t, p(toks), fail)
-		}
-	}
 }
