6a9392d18d22edc00e37dbe0f7a5f2dd298a19e3
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2016-10-06 21:56:21 -0700

    rename testing values (#18)

--- parser/lib_test.go
+++ parser/lib_test.go
@@ -8,13 +8,6 @@ import (
 	"github.com/nordsieck/defect"
 )
 
-var (
-	semicolon       = &Token{tok: token.SEMICOLON, lit: "\n"}
-	semicolonSlice  = []*Token{semicolon}
-	semicolonSlice2 = [][]*Token{semicolonSlice}
-	fail            = []*Token(nil)
-)
-
 func newScanner(src string) *scanner.Scanner {
 	var s scanner.Scanner
 	fset := token.NewFileSet()
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -5,52 +5,58 @@ import (
 	"testing"
 )
 
+var (
+	empty     = [][]*Token(nil)
+	semi      = &Token{tok: token.SEMICOLON, lit: "\n"}
+	semiSlice = [][]*Token{{semi}}
+)
+
 func TestBasicLit(t *testing.T) {
 	remaining(t, BasicLit, map[string][][]*Token{
-		``:    [][]*Token(nil),
-		`1`:   semicolonSlice2,
-		`1.1`: semicolonSlice2,
-		`1i`:  semicolonSlice2,
-		`'a'`: semicolonSlice2,
-		`"a"`: semicolonSlice2,
-		`a`:   [][]*Token(nil),
-		`_`:   [][]*Token(nil),
+		``:    empty,
+		`1`:   semiSlice,
+		`1.1`: semiSlice,
+		`1i`:  semiSlice,
+		`'a'`: semiSlice,
+		`"a"`: semiSlice,
+		`a`:   empty,
+		`_`:   empty,
 	})
 }
 
 func TestIdentifierList(t *testing.T) {
 	remaining(t, IdentifierList, map[string][][]*Token{
-		`a`:   semicolonSlice2,
-		`a,a`: semicolonSlice2,
-		`1`:   [][]*Token(nil),
-		`_`:   semicolonSlice2,
+		`a`:   semiSlice,
+		`a,a`: semiSlice,
+		`1`:   empty,
+		`_`:   semiSlice,
 	})
 }
 
 func TestTypeName(t *testing.T) {
 	remaining(t, TypeName, map[string][][]*Token{
-		`a`:   semicolonSlice2,
-		`a.a`: [][]*Token{semicolonSlice, {semicolon, {tok: token.IDENT, lit: "a"}, {tok: token.PERIOD}}},
-		`1`:   [][]*Token(nil),
-		`_`:   semicolonSlice2,
+		`a`:   semiSlice,
+		`a.a`: [][]*Token{{semi}, {semi, {tok: token.IDENT, lit: "a"}, {tok: token.PERIOD}}},
+		`1`:   empty,
+		`_`:   semiSlice,
 	})
 }
 
 func TestQualifiedIdent(t *testing.T) {
 	remaining(t, QualifiedIdent, map[string][][]*Token{
-		`1`:   [][]*Token(nil),
-		`a`:   [][]*Token(nil),
-		`a.a`: semicolonSlice2,
-		`_.a`: [][]*Token(nil),
-		`a._`: semicolonSlice2,
-		`_._`: [][]*Token(nil),
+		`1`:   empty,
+		`a`:   empty,
+		`a.a`: semiSlice,
+		`_.a`: empty,
+		`a._`: semiSlice,
+		`_._`: empty,
 	})
 }
 
 func TestPackageName(t *testing.T) {
 	remaining(t, PackageName, map[string][][]*Token{
-		`a`: semicolonSlice2,
-		`1`: [][]*Token(nil),
-		`_`: [][]*Token(nil),
+		`a`: semiSlice,
+		`1`: empty,
+		`_`: empty,
 	})
 }
